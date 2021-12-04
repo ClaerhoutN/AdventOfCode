e90f25da70cdc0580a8704b6535298747b96fe8d
Nick Claerhout <n_ick_93@hotmail.com>
2021-12-04 04:27:15 +0100

    AOC_2020_5 and AOC_2020_5_2 refactor

--- AOC_2020_5/Program.cs
+++ AOC_2020_5/Program.cs
@@ -11,18 +11,16 @@ namespace AOC_2020_5
         {
             string inputUrl = "https://adventofcode.com/2020/day/5/input";
 
-            long maxSeatId = (await InputHelper.GetInputLines<string>(inputUrl)).Max(x =>
-            {
-                x = x
-                .Replace('F', '0')
-                .Replace('B', '1')
-                .Replace('L', '0')
-                .Replace('R', '1');
-
-                long row = Convert.ToInt64(x.Substring(0, 7), 2);
-                long col = Convert.ToInt64(x.Substring(7, 3), 2);
-                return row * 8 + col;
-            });
+            string maxSeatName = (await InputHelper.GetInputLines<string>(inputUrl))
+                .Select(x => x
+                    .Replace('F', '0')
+                    .Replace('B', '1')
+                    .Replace('L', '0')
+                    .Replace('R', '1'))
+                .Max();            
+            long row = Convert.ToInt64(maxSeatName.Substring(0, 7), 2);
+            long col = Convert.ToInt64(maxSeatName.Substring(7, 3), 2);
+            long maxSeatId = row * 8 + col;
 
             Console.WriteLine(maxSeatId);
             Console.ReadKey();
--- AOC_2020_5_2/Program.cs
+++ AOC_2020_5_2/Program.cs
@@ -11,35 +11,29 @@ namespace AOC_2020_5_2
         {
             string inputUrl = "https://adventofcode.com/2020/day/5/input";
 
-            var seatIds = (await InputHelper.GetInputLines<string>(inputUrl)).Select(x =>
-            {
-                x = x
-                .Replace('F', '0')
-                .Replace('B', '1')
-                .Replace('L', '0')
-                .Replace('R', '1');
-
-                long row = Convert.ToInt64(x.Substring(0, 7), 2);
-                long col = Convert.ToInt64(x.Substring(7, 3), 2);
-                return row * 8 + col;
-            }).ToList();
-
             long maxSeatId = -1;
-            seatIds.OrderBy(x => x).ForEach((i, s, @break) =>
-            {
-                if (maxSeatId == -1)
-                    maxSeatId = s;
-                else
+            foreach (long seatId in (await InputHelper.GetInputLines<string>(inputUrl))
+                .Select(x =>
                 {
-                    if (s - maxSeatId == 2)
-                    {
-                        maxSeatId = s - 1;
-                        @break();
-                    }
-                    else
-                        maxSeatId = s;
+                    x = x
+                    .Replace('F', '0')
+                    .Replace('B', '1')
+                    .Replace('L', '0')
+                    .Replace('R', '1');                    
+                    long row = Convert.ToInt64(x.Substring(0, 7), 2);
+                    long col = Convert.ToInt64(x.Substring(7, 3), 2);                    
+                    return row * 8 + col;
+                })
+                .OrderBy(x => x))
+            {
+                if (maxSeatId != -1 && seatId - maxSeatId == 2)
+                {
+                    maxSeatId = seatId - 1;
+                    break;
                 }
-            });
+                else
+                    maxSeatId = seatId;
+            }
 
             Console.WriteLine(maxSeatId);
             Console.ReadKey();
