a9dfdb250ad234ca509a557e80808a4d737e5827
Nick Claerhout <n_ick_93@hotmail.com>
2021-12-04 17:12:58 +0100

    AOC_2020_6 and AOC_2020_6_2

---
+++ AOC_2020_6/Program.cs
@@ -0,0 +1,24 @@
+﻿using AOC.Util;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AOC_2020_6
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            string inputUrl = "https://adventofcode.com/2020/day/6/input";
+
+            List<int> yesAnswers = (await InputHelper.GetInputLines<string>(inputUrl, lineSeparatorRegex: "\\n\\n"))
+                .Select(x => x.Where(y => y != '\n').Distinct().Count())
+                .ToList();
+
+
+            Console.WriteLine(yesAnswers.Sum());
+            Console.ReadKey();
+        }
+    }
+}
---
+++ AOC_2020_6_2/Program.cs
@@ -0,0 +1,25 @@
+﻿using AOC.Util;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AOC_2020_6_2
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            string inputUrl = "https://adventofcode.com/2020/day/6/input";
+
+            List<int> yesAnswers = (await InputHelper.GetInputLines<string[]>(inputUrl, 
+                argumentSeparatorRegex: "\\n", lineSeparatorRegex: "\\n\\n"))
+                .Select(x => Enumerable.Range('a', 'z').Count(q => x.All(p => p.Contains((char)q))))
+                .ToList();
+
+
+            Console.WriteLine(yesAnswers.Sum());
+            Console.ReadKey();
+        }
+    }
+}
