ba116c103899c7b80b6c5fcab73f44d2c9d228d1
Nick Claerhout <n_ick_93@hotmail.com>
2021-12-05 23:19:56 +0100

    AOC_2020_10 - part 2

--- AOC_2020_10/main.cpp
+++ AOC_2020_10/main.cpp
@@ -1,17 +1,45 @@
 #include "input.h"
 #include <iostream>
 #include <algorithm>
+#include <unordered_map>
 #include <stringUtilities.h>
 using namespace std;
 
-void AddJoltageInChain(vector<int>& const joltages, int fromIndex, vector<int>& const joltageDifferences, bool all = true)
+void addJoltageDiffInChain(vector<int>& const joltages, int fromIndex, vector<int>& const joltageDifferences)
 {
 	if (fromIndex +1 >= joltages.size()) return;
-	if (all)
+	joltageDifferences[fromIndex] = joltages[fromIndex + 1] - joltages[fromIndex];
+	addJoltageDiffInChain(joltages, fromIndex + 1, joltageDifferences);
+}
+
+unsigned long long countAdapterChains(vector<int>& const joltages, int fromIndex, int lastJoltage)
+{
+	static unordered_map<int, unsigned long long> cacheByLastJoltage;
+	if (fromIndex == joltages.size() - 1)
+		return 1;
+	if (lastJoltage == -1)
 	{
-		joltageDifferences[fromIndex] = joltages[fromIndex + 1] - joltages[fromIndex];
-		AddJoltageInChain(joltages, fromIndex + 1, joltageDifferences);
+		lastJoltage = 0;
+		fromIndex = 1;
 	}
+
+	unsigned long long count = 0;
+	for (int jIndex = fromIndex; jIndex < joltages.size(); ++jIndex)
+	{
+		if (joltages[jIndex] - lastJoltage <= 3)
+		{
+			if (cacheByLastJoltage.find(joltages[jIndex]) != cacheByLastJoltage.end())
+				count += cacheByLastJoltage[joltages[jIndex]];
+			else if (jIndex == joltages.size() - 1)
+				++count;
+			else
+				count += countAdapterChains(joltages, jIndex + 1, joltages[jIndex]);
+		}
+		else
+			break;
+	}
+	cacheByLastJoltage[lastJoltage] = count;
+	return count;
 }
 
 int main() {
@@ -25,12 +53,14 @@ int main() {
 	joltages.push_back(joltages.back() + 3);
 
 	vector<int> joltageDifferences(static_cast<size_t>(joltages.size() - 1));	
-	AddJoltageInChain(joltages, 0, joltageDifferences);
-
+	addJoltageDiffInChain(joltages, 0, joltageDifferences);
 	int count1j = count_if(joltageDifferences.begin(), joltageDifferences.end(), [](int j){return j == 1;});
 	int count3j = count_if(joltageDifferences.begin(), joltageDifferences.end(), [](int j){return j == 3;});
 
-	cout << count1j * count3j << endl;
+	unsigned long long adapterChainCnt = countAdapterChains(joltages, 0, -1);
+
+	cout << "part 1: " << count1j * count3j << endl;
+	cout << "part 2: " << adapterChainCnt << endl;
 	cin.get();
 	return 0;
 }
