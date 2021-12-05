c04da68529f47ca57467b0618b9e50267c0af431
Nick Claerhout <n_ick_93@hotmail.com>
2021-12-05 03:40:44 +0100

    AOC_2020_8

---
+++ AOC_2020_8/main.cpp
@@ -0,0 +1,35 @@
+#include "input.h"
+#include <iostream>
+#include <stringUtilities.h>
+#include <tuple>
+using namespace std;
+
+int main() {
+
+	vector<tuple<string, int, bool>> commands;
+	for (string& instruction : split(input, '\n'))
+	{
+		string name = instruction.substr(0, 3);
+		int amount = stoi(instruction.substr(4));
+		commands.push_back(make_tuple(name, amount, false));
+	}
+
+	tuple<string, int, bool>* instructionPtr = &commands[0];
+	int acc = 0;
+	do
+	{
+		string name = get<0>(*instructionPtr);
+		int amount = get<1>(*instructionPtr);
+		get<2>(*instructionPtr) = true;
+		if (name == "acc")
+			acc += amount;
+		if (name == "jmp")
+			instructionPtr += amount;
+		else
+			++instructionPtr;
+	} while (!get<2>(*instructionPtr));
+
+	cout << acc << endl;
+	cin.get();
+	return 0;
+}
