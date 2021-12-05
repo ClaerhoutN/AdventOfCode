8d8671642675d390f9423f93241e2f6658ca4ad4
Nick Claerhout <n_ick_93@hotmail.com>
2021-12-05 04:48:18 +0100

    AOC_2020_8_2

---
+++ AOC_2020_8_2/main.cpp
@@ -0,0 +1,61 @@
+#include "input.h"
+#include <iostream>
+#include <stringUtilities.h>
+#include <tuple>
+using namespace std;
+
+int main() {
+
+	vector<tuple<string, int, bool>> commands;
+	for (string& instruction : split(input, '\n'))
+	{
+		string name = instruction.substr(0, 3);
+		int amount = stoi(instruction.substr(4));
+		commands.push_back(make_tuple(name, amount, false));
+	}
+
+	int acc;
+	for (auto& command : commands)
+	{
+		if (get<0>(command) == "acc") continue;
+
+		string oldSwitch = get<0>(command);
+		get<0>(command) = (oldSwitch == "nop" ? "jmp" : "nop");
+		bool found = false;
+		auto* instructionPtr = &commands.front();
+		acc = 0;
+
+		do
+		{
+			string name = get<0>(*instructionPtr);
+			int amount = get<1>(*instructionPtr);
+			get<2>(*instructionPtr) = true;
+
+			if (name == "acc")
+				acc += amount;
+			if (name == "jmp")
+				instructionPtr += amount;
+			else
+				++instructionPtr;
+
+			if (instructionPtr == &commands.back())
+				found = true;
+			else if (instructionPtr < &commands.front() || instructionPtr > &commands.back())
+				break;
+		} while (!found && !get<2>(*instructionPtr));
+		
+		get<0>(command) = oldSwitch;
+
+		for (auto& c : commands)
+		{
+			get<2>(c) = false;
+		}
+
+		if (found)
+			break;
+	}
+
+	cout << acc << endl;
+	cin.get();
+	return 0;
+}
