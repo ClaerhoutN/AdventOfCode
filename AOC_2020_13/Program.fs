693d6d8e655607100de98f29863a9cf753313ec8
Nick Claerhout <n_ick_93@hotmail.com>
2021-12-09 19:54:28 +0100

    AOC_2020_13

---
+++ AOC_2020_13/Program.fs
@@ -0,0 +1,21 @@
+﻿open System
+open AOC.Util
+
+let getFirstReachableBus timestamp busses = 
+    busses 
+    |> Seq.map (fun bus -> bus, bus * int (ceil (float timestamp / float bus)))
+    |> Seq.minBy (fun (bus, ts) -> ts)
+
+[<EntryPoint>]
+let main argv =
+    let input = InputHelper.GetInputLines<string>("https://adventofcode.com/2020/day/13/input")
+                        |> Async.AwaitTask |> Async.RunSynchronously
+                        |> List.ofSeq
+    let timestampReady = int input.[0]
+    let allBusses = input.[1].Split(',') 
+                    |> Seq.filter (fun bus -> bus <> "x") 
+                    |> Seq.map (fun bus -> int bus)
+                    |> List.ofSeq
+    let (bus, timestampDeparture) = getFirstReachableBus timestampReady allBusses                            
+    printfn "%i" (bus * (timestampDeparture - timestampReady))
+    0
