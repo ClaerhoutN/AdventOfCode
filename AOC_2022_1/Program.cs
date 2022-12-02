e974d182a5cabfd036e8363fe94f9ca7a237066c
Nick Claerhout <n_ick_93@hotmail.com>
2022-12-02 02:46:43 +0100

    AOC_2022_1 - part 1 and 2

---
+++ AOC_2022_1/Program.cs
@@ -0,0 +1,22 @@
+﻿using AOC.Util;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AOC_2022_1
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            string inputUrl = "https://adventofcode.com/2022/day/1/input";
+
+            int maxCaloriesSum = (await InputHelper.GetInputLines<int[]>(inputUrl, "\n", "\n\n"))
+                .Select(x => x.Sum())
+                .OrderByDescending(x => x)
+                .Take(3)
+                .Sum();
+            Console.WriteLine(maxCaloriesSum);
+        }
+    }
+}
