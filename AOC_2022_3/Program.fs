b2bbf826d24bb8952a82a4cac40705e966da0445
Nick Claerhout <n_ick_93@hotmail.com>
2022-12-03 20:45:57 +0100

    AOC_2022_3 refactoring

--- AOC_2022_3/Program.fs
+++ AOC_2022_3/Program.fs
@@ -1,14 +1,11 @@
 ﻿open System
 
-let priority item = int item - (if item >= 'a' then 96 else 38)
-
 [<EntryPoint>]
 let main argv =
     let input = AOC.Util.InputHelper.GetInputLines<string>("https://adventofcode.com/2022/day/3/input", lineSeparatorRegex = "\n") |> Async.AwaitTask |> Async.RunSynchronously
-    let compartmentIntersectionPrioritySum = input |> Seq.map (fun s -> [s.[..s.Length/2-1]|> Set.ofSeq; s.[s.Length/2..]|> Set.ofSeq] |> Set.intersectMany |> Seq.head)
-                                                   |> Seq.sumBy priority
-    let badgePrioritySum = seq { for i in 0..3..input.Count-3 -> Set.intersectMany [Set.ofSeq input.[i]; Set.ofSeq input.[i+1]; Set.ofSeq input.[i+2]] |> Seq.head }
-                           |> Seq.sumBy priority
+    let priority item = int item - (if item >= 'a' then 96 else 38)
+    let compartmentIntersectionPrioritySum = input |> Seq.sumBy (fun s -> Seq.splitInto 2 s |> Seq.map Set.ofSeq |> Set.intersectMany |> Seq.head |> priority)
+    let badgePrioritySum = Seq.sumBy priority (seq { for i in 0..3..input.Count-3 -> Set.intersectMany [for i2 in 0..2 -> Set.ofSeq input.[i+i2]] |> Seq.head })
                 
     printfn "%i %i" compartmentIntersectionPrioritySum badgePrioritySum
-    0 // return an integer exit code
+    0
