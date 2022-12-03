188edf2eae075431efc38d55a72e11c3084515b9
Nick Claerhout <n_ick_93@hotmail.com>
2022-12-03 20:20:59 +0100

    AOC_2022_3 - part 1 and 2

---
+++ AOC_2022_3/Program.fs
@@ -0,0 +1,14 @@
+﻿open System
+
+let priority item = int item - (if item >= 'a' then 96 else 38)
+
+[<EntryPoint>]
+let main argv =
+    let input = AOC.Util.InputHelper.GetInputLines<string>("https://adventofcode.com/2022/day/3/input", lineSeparatorRegex = "\n") |> Async.AwaitTask |> Async.RunSynchronously
+    let compartmentIntersectionPrioritySum = input |> Seq.map (fun s -> [s.[..s.Length/2-1]|> Set.ofSeq; s.[s.Length/2..]|> Set.ofSeq] |> Set.intersectMany |> Seq.head)
+                                                   |> Seq.sumBy priority
+    let badgePrioritySum = seq { for i in 0..3..input.Count-3 -> Set.intersectMany [Set.ofSeq input.[i]; Set.ofSeq input.[i+1]; Set.ofSeq input.[i+2]] |> Seq.head }
+                           |> Seq.sumBy priority
+                
+    printfn "%i %i" compartmentIntersectionPrioritySum badgePrioritySum
+    0 // return an integer exit code
