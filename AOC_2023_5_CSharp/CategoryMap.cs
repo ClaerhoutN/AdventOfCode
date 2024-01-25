a9d5b0855ec08d7f6f287cad527b05ccadde0783
Nick Claerhout <claerhout.nick@gmail.com>
2024-01-25 23:37:48 +0100

    AOC 2023 day 5 part 1 (C# + excel)

---
+++ AOC_2023_5_CSharp/CategoryMap.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AOC_2023_5_CSharp
+{
+    internal class CategoryMap
+    {
+        readonly struct NumberRange(long source, long destination, long length)
+        {
+            public readonly long Source = source;
+            public readonly long Destination = destination;
+            public readonly long Length = length;
+        }
+        private readonly List<NumberRange> _numberRanges = new List<NumberRange>();
+        public void Add(long source, long destination, long length)
+        {
+            _numberRanges.Add(new NumberRange(source, destination, length));
+        }
+        public long GetDestinationCategory(long sourceCategory)
+        {
+            foreach(var range in _numberRanges)
+            {
+                long diff = sourceCategory - range.Source;
+                if (diff >= 0 && diff <= range.Length)
+                    return range.Destination + diff;
+            }
+            return sourceCategory;
+        }
+    }
+}
---
+++ AOC_2023_5_CSharp/Program.cs
@@ -0,0 +1,33 @@
+﻿using AOC.Util;
+using AOC_2023_5_CSharp;
+
+string inputUrl = "https://adventofcode.com/2023/day/5/input";
+
+var inputLines = await InputHelper.GetInputLines<string[]>(inputUrl, argumentSeparatorRegex: " ");
+IReadOnlyList<long> seeds = inputLines[0].Skip(1).Select(x => long.Parse(x)).ToList();
+
+List<CategoryMap> maps = new List<CategoryMap>();
+CategoryMap map = null;
+foreach(var inputLine in inputLines.Skip(1))
+{
+    if (inputLine.Last() == "map:")
+    {
+        map = new CategoryMap();
+        maps.Add(map);
+        continue;
+    }
+    map.Add(long.Parse(inputLine[1]), long.Parse(inputLine[0]), long.Parse(inputLine[2]));
+}
+
+Console.WriteLine("lowest location number: " + seeds.Min(GetLocationNumber));
+Console.ReadKey();
+
+long GetLocationNumber(long seed)
+{
+    long destinationCategory = seed;
+    foreach(var map in maps)
+    {
+        destinationCategory = map.GetDestinationCategory(destinationCategory);
+    }
+    return destinationCategory;
+}
