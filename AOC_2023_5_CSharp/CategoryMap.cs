185467f3c0dbc21bd34685efe7c02082648cf21e
Nick Claerhout <claerhout.nick@gmail.com>
2024-01-27 23:11:26 +0100

    code cleanup

--- AOC_2023_5_CSharp/CategoryMap.cs
+++ AOC_2023_5_CSharp/CategoryMap.cs
@@ -1,10 +1,4 @@
 ﻿using AOC.Util;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using static AOC_2023_5_CSharp.CategoryMap;
 
 namespace AOC_2023_5_CSharp
 {
