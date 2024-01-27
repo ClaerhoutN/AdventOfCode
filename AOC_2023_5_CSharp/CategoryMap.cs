fd39cf722828ba2f4d867a8c9d3dfb31d99254b9
Nick Claerhout <claerhout.nick@gmail.com>
2024-01-27 23:10:40 +0100

    AOC 2023 day 5 part 2

--- AOC_2023_5_CSharp/CategoryMap.cs
+++ AOC_2023_5_CSharp/CategoryMap.cs
@@ -1,33 +1,74 @@
-﻿using System;
+﻿using AOC.Util;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static AOC_2023_5_CSharp.CategoryMap;
 
 namespace AOC_2023_5_CSharp
 {
     internal class CategoryMap
     {
-        readonly struct NumberRange(long source, long destination, long length)
+        public readonly struct CategoryRange(long source, long destination, long length)
         {
             public readonly long Source = source;
             public readonly long Destination = destination;
             public readonly long Length = length;
         }
-        private readonly List<NumberRange> _numberRanges = new List<NumberRange>();
+        private readonly List<CategoryRange> _categoryRanges = new List<CategoryRange>();
         public void Add(long source, long destination, long length)
         {
-            _numberRanges.Add(new NumberRange(source, destination, length));
+            _categoryRanges.Add(new CategoryRange(source, destination, length));
         }
         public long GetDestinationCategory(long sourceCategory)
         {
-            foreach(var range in _numberRanges)
+            foreach(var range in _categoryRanges)
             {
                 long diff = sourceCategory - range.Source;
-                if (diff >= 0 && diff <= range.Length)
+                if (diff >= 0 && diff < range.Length)
                     return range.Destination + diff;
             }
             return sourceCategory;
+        }
+        public IReadOnlyList<(long, long)> GetDestinationCategoryRanges((long sourceCategory, long sourceRange) sourceCategoryRange)
+        {
+            List<(long, long)> destinationCategoryRanges = new List<(long, long)>();
+            long sourceCategory = sourceCategoryRange.sourceCategory;
+            long sourceRange = sourceCategoryRange.sourceRange;
+
+            var sortedCategoryRanges = _categoryRanges.OrderBy(x => x.Source).ToList();
+            while (sourceRange > 0)
+            {
+                bool found = false;
+                foreach (var categoryRange in sortedCategoryRanges)
+                {
+                    long diff = sourceCategory - categoryRange.Source;
+                    if (diff >= 0 && diff < categoryRange.Length)
+                    {
+                        long destination = categoryRange.Destination + diff;
+                        long length = Math.Min(categoryRange.Length - diff, sourceRange);
+                        destinationCategoryRanges.Add((destination, length));
+                        found = true;
+                        sourceCategory += length;
+                        sourceRange -= length;
+                        break;
+                    }
+                }
+                if(!found)
+                {
+                    long destination = sourceCategory, length;
+                    int nextCategoryRangeIndex = sortedCategoryRanges.IndexWhere(x => x.Source > sourceCategory);
+                    if(nextCategoryRangeIndex == -1) //no more number ranges will cover the remaining sourceCategoryRange
+                        length = sourceRange;
+                    else
+                        length = sortedCategoryRanges[nextCategoryRangeIndex].Source - sourceCategory;
+                    destinationCategoryRanges.Add((destination, length));
+                    sourceCategory += length;
+                    sourceRange -= length;
+                }
+            }
+            return destinationCategoryRanges;
         }
     }
 }
--- AOC_2023_5_CSharp/Program.cs
+++ AOC_2023_5_CSharp/Program.cs
@@ -2,9 +2,7 @@
 using AOC_2023_5_CSharp;
 
 string inputUrl = "https://adventofcode.com/2023/day/5/input";
-
 var inputLines = await InputHelper.GetInputLines<string[]>(inputUrl, argumentSeparatorRegex: " ");
-IReadOnlyList<long> seeds = inputLines[0].Skip(1).Select(x => long.Parse(x)).ToList();
 
 List<CategoryMap> maps = new List<CategoryMap>();
 CategoryMap map = null;
@@ -19,7 +17,22 @@ foreach(var inputLine in inputLines.Skip(1))
     map.Add(long.Parse(inputLine[1]), long.Parse(inputLine[0]), long.Parse(inputLine[2]));
 }
 
-Console.WriteLine("lowest location number: " + seeds.Min(GetLocationNumber));
+#region part 1
+IReadOnlyList<long> seeds = inputLines[0].Skip(1).Select(long.Parse).ToList();
+Console.WriteLine("Part 1 - lowest location number: " + seeds.Min(GetLocationNumber));
+#endregion part 1
+
+#region part 2
+List<(long, long)> seedRanges = new List<(long, long)>();
+for(int i = 0; i < seeds.Count-1; i+= 2)
+{
+    seedRanges.Add((seeds[i], seeds[i + 1]));
+}
+Console.WriteLine("Part 2 - lowest location number: " 
+    + seedRanges.Min(x => GetLocationNumberRanges(x).Select(y => y.Item1).Min()));
+#endregion part 2
+
+
 Console.ReadKey();
 
 long GetLocationNumber(long seed)
@@ -30,4 +43,23 @@ long GetLocationNumber(long seed)
         destinationCategory = map.GetDestinationCategory(destinationCategory);
     }
     return destinationCategory;
+}
+IReadOnlyList<(long, long)> GetLocationNumberRanges((long, long) seedRange)
+{
+    List<(long, long)> destinationCategoryRanges = new List<(long, long)>() { seedRange };
+    int i = 0;
+    foreach (var map in maps)
+    {
+        ++i;
+
+        var destinationCategoryRangesCopy = new List<(long, long)>(destinationCategoryRanges);
+        destinationCategoryRanges.Clear();
+        foreach (var destinationCategoryRange in destinationCategoryRangesCopy)
+        {
+            var t = map.GetDestinationCategoryRanges(destinationCategoryRange);
+            destinationCategoryRanges.AddRange(t);
+        }
+    }
+
+    return destinationCategoryRanges;
 }
